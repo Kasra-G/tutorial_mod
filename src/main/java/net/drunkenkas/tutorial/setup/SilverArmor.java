972e69982caa56a63878a3961fd4a092c19ab7c6
Kasra Ghaffari <ks.ghaffari1@gmail.com>
2021-06-15 20:57:26 -0400

    Rearranged SilverArmor.java

--- src/main/java/net/drunkenkas/tutorial/setup/SilverArmor.java
+++ src/main/java/net/drunkenkas/tutorial/setup/SilverArmor.java
@@ -11,16 +11,21 @@ import net.minecraft.world.World;
 public class SilverArmor extends ArmorItem {
     private static final EffectInstance SILVER_BOOTS_EFFECT_INSTANCE = new EffectInstance(ModEffects.MOVEMENT_SPEED, 1);
     private static final EffectInstance SILVER_HELMET_EFFECT_INSTANCE = new EffectInstance(Effects.NIGHT_VISION, 1);
+
     public SilverArmor(EquipmentSlotType equipmentSlotType, Properties properties) {
         super(ModArmorMaterial.SILVER, equipmentSlotType, properties);
     }
 
     @Override
     public void onArmorTick(ItemStack stack, World world, PlayerEntity player) {
-        if (stack.getItem().equals(ModItems.SILVER_BOOTS.get())) {
-            player.addEffect(SILVER_BOOTS_EFFECT_INSTANCE);
-        } else if (stack.getItem().equals(ModItems.SILVER_HELMET.get())) {
+        if (stack.getItem().equals(ModItems.SILVER_HELMET.get())) {
             player.addEffect(SILVER_HELMET_EFFECT_INSTANCE);
+        } else if (stack.getItem().equals(ModItems.SILVER_CHESTPLATE.get())) {
+
+        } else if (stack.getItem().equals(ModItems.SILVER_LEGGINGS.get())) {
+
+        } else if (stack.getItem().equals(ModItems.SILVER_BOOTS.get())) {
+            player.addEffect(SILVER_BOOTS_EFFECT_INSTANCE);
         }
     }
 }
