a85528f82f76d682c24f6c0ffa03971d25c1847a
Kasra Ghaffari <ks.ghaffari1@gmail.com>
2021-06-17 19:16:26 -0400

    SilverfishDropModifier javadoc added

--- src/main/java/net/drunkenkas/tutorial/modifier/SilverfishDropModifier.java
+++ src/main/java/net/drunkenkas/tutorial/modifier/SilverfishDropModifier.java
@@ -16,14 +16,20 @@ import java.util.List;
 import java.util.Objects;
 import java.util.Random;
 
+/**
+ * Modifies the loot dropped by the Silverfish on death.
+ * The item dropped and the chance of drop is configurable.
+ */
 public class SilverfishDropModifier extends LootModifier {
     private final float dropChance;
     private final Item dropItem;
 
     /**
-     * Constructs a LootModifier.
+     * Constructs a SilverfishDropModifier.
      *
      * @param conditionsIn the ILootConditions that need to be matched before the loot is modified.
+     * @param dropItem the Item to be dropped by the Silverfish on death
+     * @param dropChance the float chance of dropping the Item by the Silverfish on death
      */
     public SilverfishDropModifier(ILootCondition[] conditionsIn, final Item dropItem, final float dropChance) {
         super(conditionsIn);
@@ -31,6 +37,13 @@ public class SilverfishDropModifier extends LootModifier {
         this.dropChance = dropChance;
     }
 
+    /**
+     * Applies the modifier.
+     *
+     * @param generatedLoot the loot generated already
+     * @param context the LootContext of this loot generation
+     * @return the updated loot
+     */
     @Nonnull
     @Override
     protected List<ItemStack> doApply(List<ItemStack> generatedLoot, LootContext context) {
@@ -38,9 +51,18 @@ public class SilverfishDropModifier extends LootModifier {
         return generatedLoot;
     }
 
+    /**
+     * Gets the appropriate loot for the Silverfish to drop.
+     * Also accounts for looting.
+     *
+     * @param context the LootContext of this loot generation
+     * @return the added loot for the Silverfish
+     */
     private ItemStack getLoot(LootContext context) {
         Random randy = context.getRandom();
         double amount = 0;
+
+        //account for looting by just reapplying the drop for every level of looting.
         for (int i = 0; i < context.getLootingModifier() + 1; i++) {
             if (randy.nextDouble() <= dropChance) {
                 amount++;
@@ -49,8 +71,20 @@ public class SilverfishDropModifier extends LootModifier {
         return new ItemStack(dropItem, (int) amount);
     }
 
+    /**
+     * Serializer for the SilverfishDropModifier.
+     * Created so that the class can read and write from the Loot modifier JSON file.
+     */
     public static class Serializer extends GlobalLootModifierSerializer<SilverfishDropModifier> {
 
+        /**
+         * Reads the dropItem and dropChance from the JSON file.
+         *
+         * @param location the ResourceLocation of the serializer (unused)
+         * @param object the JsonObject for the JSON file for the SilverfishDropModifier
+         * @param conditionsIn the loot conditions
+         * @return the new SilverfishDropModifier constructed from the JSON.
+         */
         @Override
         public SilverfishDropModifier read(ResourceLocation location, JsonObject object, ILootCondition[] conditionsIn) {
             Item dropItem = ForgeRegistries.ITEMS.getValue(new ResourceLocation(JSONUtils.getAsString(object, "dropItem")));
@@ -58,6 +92,12 @@ public class SilverfishDropModifier extends LootModifier {
             return new SilverfishDropModifier(conditionsIn, dropItem, dropChance);
         }
 
+        /**
+         * Encodes the given instance of SilvefishDropModifier as a JsonObject.
+         *
+         * @param instance the instance of SilverfishDropModifier to encode
+         * @return the encoded JsonObject
+         */
         @Override
         public JsonObject write(SilverfishDropModifier instance) {
             JsonObject json = makeConditions(instance.conditions);
