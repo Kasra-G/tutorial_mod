97f95d6e5090cfe94b23459cbde4e4dc7eb57cd8
Kasra Ghaffari <ks.ghaffari1@gmail.com>
2021-06-17 19:21:20 -0400

    ModLootModiferProvider javadoc added

--- src/main/java/net/drunkenkas/tutorial/data/ModLootModiferProvider.java
+++ src/main/java/net/drunkenkas/tutorial/data/ModLootModiferProvider.java
@@ -10,14 +10,31 @@ import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.common.data.GlobalLootModifierProvider;
 import net.minecraftforge.common.loot.LootTableIdCondition;
 
+/**
+ * This class contains all the loot modifications done.
+ */
 public class ModLootModiferProvider extends GlobalLootModifierProvider {
 
+    /**
+     * Instantiates this LootModifierProvider.
+     *
+     * @param gen the generator to use
+     */
     public ModLootModiferProvider(DataGenerator gen) {
         super(gen, TutorialMod.MOD_ID);
     }
 
+    /**
+     * Adds all the loot modifications.
+     *
+     * Modified loot: <p>
+     *     Silverfish drop silver dust on death.
+     * </p>
+     */
     @Override
     protected void start() {
+
+        //make silverfish drop silver dust with a 39% chance
         add("silverfish_drop", ModLootModifiers.SILVER_FISH_DROPS.get(), new SilverfishDropModifier(
                 new ILootCondition[] {
                         LootTableIdCondition.builder(new ResourceLocation("entities/silverfish")).build()
